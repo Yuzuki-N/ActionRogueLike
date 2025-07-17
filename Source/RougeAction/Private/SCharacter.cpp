54f1b49df147659fa2178ef8a6fe4ecd2c3a8432
Yuzuki-N <59171699+Yuzuki-N@users.noreply.github.com>
2025-07-17 08:54:41 +0800

    断言

--- Source/RougeAction/Private/SCharacter.cpp
+++ Source/RougeAction/Private/SCharacter.cpp
@@ -107,14 +107,17 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 void ASCharacter::PrimaryAttack_TimeElapsed()
 {
-    FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
-    FTransform SpawnTM = FTransform(GetControlRotation(), HandLocation);
+    if (ensure(ProjectileClass))
+    {
+        FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
+        FTransform SpawnTM = FTransform(GetControlRotation(), HandLocation);
 
-    FActorSpawnParameters SpawnParams;
-    SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-    SpawnParams.Instigator = this;
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        SpawnParams.Instigator = this;
 
-    GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+        GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+    }
 }
 
 
